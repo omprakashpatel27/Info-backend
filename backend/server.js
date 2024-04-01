540466a2f0b95f0c9726dbc40f937047e90c940f
Omprakash Patel <59342793+omprakashpatel27@users.noreply.github.com>
2024-04-01 16:46:55 +0530

    installed body parser & added mongo config

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,27 @@ const cors = require('cors');
 const app = express();
 
 const apiRoute = require('./routes/api');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const config = require('./config/database');
+const connection = mongoose.connect(config.database,{
+	useNewUrlParser: true, 
+	useUnifiedTopology: true
+});
 
 app.use(cors());
 
 app.use(express.json());
 
 app.use('/api', apiRoute);
+
+if(connection) console.log('database connected');
+else console.log('databse not connected');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3001;
 
